d5fd22721a4b30f638fdeb481894d7fe2f87a8bc
Stefano Romanò <rumix87@gmail.com>
2015-10-22 00:29:13 +0200

    Added first mockup for log sender using sockets Added mockups for log collector and system info monitor

--- woodpecker/logging/sender.py
+++ woodpecker/logging/sender.py
@@ -1,10 +1,20 @@
+import socket
+import json
+
 __author__ = 'Stefano.Romano'
 
 
 class Sender(object):
 
-    def __init__(self, str_receiver_url):
+    def __init__(self, str_receiver_url, int_receiver_port):
         self.receiver_url = str_receiver_url
+        self.receiver_port = int_receiver_port
 
-    def send(self, obj_data):
-        pass
+        # Create socket
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.receiver_url, self.receiver_port))
+
+    def send(self, str_data_type, dic_data):
+        dic_payload = {'dataType': str_data_type, 'payload': dic_data}
+        str_payload = json.dumps(dic_payload)
+        return self.socket.send(str_payload)
