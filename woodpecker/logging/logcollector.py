4aee4a757c6a5bb674dbf71218a213bf5847192a
Stefano Romanò <rumix87@gmail.com>
2015-10-29 15:58:41 +0100

    Finished LogCollector class Verified data insertion in SQlite DB from UDP socket Changed socket type from TCP to UDP

--- woodpecker/logging/logcollector.py
+++ woodpecker/logging/logcollector.py
@@ -6,28 +6,28 @@ from woodpecker.logging.dbwriter import DBWriter
 __author__ = 'Stefano'
 
 
-class LogCollector(SocketServer.StreamRequestHandler):
+class LogCollector(SocketServer.BaseRequestHandler):
 
-    def __init__(self, request, client_address, server):
-        self.dbwriter = DBWriter('testDB.sqlite')
-        SocketServer.StreamRequestHandler.__init__(self, request, client_address, server)
-        return
+    def setup(self):
+        self.dbwriter = DBWriter(self.server.result_file_path)
 
     def handle(self):
-        str_message = self.rfile.readline().strip()
+        str_message = self.request[0].strip()
         dic_message = json.loads(str_message)
+        dic_payload = dic_message.get('payload', {})
 
         if dic_message.get('logType') == 'transactionStart':
-            self.dbwriter.write_transaction_start(dic_message.get('payload', {}))
+            self.dbwriter.write_transaction_start(dic_payload)
         elif dic_message.get('logType') == 'transactionEnd':
-            self.dbwriter.write_transaction_end(dic_message.get('payload', {}))
+            self.dbwriter.write_transaction_end(dic_payload)
         elif dic_message.get('logType') == 'request':
-            self.dbwriter.write_request(dic_message.get('payload', {}))
+            self.dbwriter.write_request(dic_payload)
         elif dic_message.get('logType') == 'spawns':
-            self.dbwriter.write_spawns_info(dic_message.get('payload', {}))
+            self.dbwriter.write_spawns_info(dic_payload)
         elif dic_message.get('logType') == 'sysmonitor':
-            self.dbwriter.write_sysmonitor_info(dic_message.get('payload', {}))
+            self.dbwriter.write_sysmonitor_info(dic_payload)
 
 if __name__ == '__main__':
-    obj_server = SocketServer.TCPServer(('localhost', 7777), LogCollector)
+    obj_server = SocketServer.UDPServer(('localhost', 7777), LogCollector)
+    obj_server.result_file_path = "D:/Data/Varie/Programmi Python/Woodpecker/tests/results.sqlite"
     obj_server.serve_forever()
