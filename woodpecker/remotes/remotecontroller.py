157aa17d547013ebbd45ffc0f0178029ff9f5956
Stefano Romanò <rumix87@gmail.com>
2015-11-03 17:51:18 +0100

    Started creating the remote controller part

---
+++ woodpecker/remotes/remotecontroller.py
@@ -0,0 +1,10 @@
+import SocketServer
+
+from woodpecker.remotes.spawn import Spawn
+from woodpecker.remotes.sysmonitor import Sysmonitor
+
+__author__ = 'romano'
+
+
+class RemoteController(SocketServer.BaseRequestHandler):
+    pass
--- woodpecker/remotes/spawner.py
+++ woodpecker/remotes/spawner.py
@@ -1,45 +1,80 @@
-from woodpecker.misc.utils import get_timestamp, random_id
+import tempfile
+import base64
+import woodpecker.misc.utils as utils
+
 from woodpecker.remotes.spawn import Spawn
+from woodpecker.logging.sender import Sender
+from woodpecker.misc.stoppablethread import StoppableThread
 
 __author__ = 'Stefano.Romano'
 
 
-class Spawner(object):
+class Spawner(StoppableThread):
 
-    def __init__(self, str_scenario_path, str_host='', str_port=7777):
-        self.host = str_host
-        self.port = str_port
-        # self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.elapsed_time = 0
-        self.armed = False
+    def __init__(self, str_server_address, str_base64_scenario_serialization):
+        # Initialize the father stoppable thread class
+        super(Spawner, self).__init__()
 
-        obj_module = __import__(str_scenario_path)
-        obj_class = getattr(obj_module, str_scenario_path)
-        self.scenario = obj_class()
-
-    def arm(self):
-        # self.socket.bind((self.host, self.port))
-        # self.socket.listen(5)
-        self.armed = True
+        # Instantiates everything
+        self.__initialize(server_address=str_server_address)
+        # self.__set_scenario_class(str_base64_scenario_serialization)
+        self.__arm()
 
     def run(self):
+        self.__run()
+
+    def __initialize(self, **kwargs):
+        # Controller and Log collector socket port
+        self.port = kwargs.get('port', 7878)
+
+        #  Controller address, void at the beginning
+        self.server_address = kwargs.get('server_address', None)
+
+        # Working dir, defaults to standard temp folder (depending on OS)
+        self.temp_folder = tempfile.gettempdir()
+
+        # Placeholder for scenario
+        self.scenario = None
+
+        # Armed flag, used to start and stop running
+        self.armed = False
+
+        # Elapsed time since scenario beginning
+        self.elapsed_time = 0
+
+        # Internal message sender placeholder
+        self.sender = Sender(self.server_address, self.port, 'UDP') if self.server_address else None
+
+    def __set_scenario_class(self, str_base64_scenario_serialization):
+        # Load scenario from ZIP file
+        pass
+
+    def __arm(self):
+        # Arm itself
+        self.armed = True
+
+    def __unarm(self):
+        # Disarm itself
+        self.armed = False
+
+    def __run(self):
         self.scenario.configure()
         self.scenario.tests_definition()
 
-        self.scenario.scenario_start = get_timestamp(False)
+        self.scenario.scenario_start = utils.get_timestamp(False)
         self.scenario.scenario_duration =\
             self.scenario.get_scenario_duration()
         list_tests = self.scenario.get_test_names()
 
-        while self.elapsed_time <= self.scenario.scenario_duration:
-            time_elapsed = get_timestamp(False) - self.scenario.scenario_start
+        while self.elapsed_time <= self.scenario.scenario_duration and self.armed:
+            time_elapsed = utils.get_timestamp(False) - self.scenario.scenario_start
             self.elapsed_time = time_elapsed.total_seconds()
             for str_test_name in list_tests:
                 self.scenario.tests[str_test_name]['planned_spawns'] =\
                     self.scenario.get_planned_spawns(str_test_name, self.elapsed_time)
 
                 self.scenario.tests[str_test_name]['current_spawns'] =\
-                    len(self.scenario.tests[str_test_name]['threads'])
+                    len(self.scenario.tests[str_test_name]['spawns'])
 
                 int_spawns_difference = self.scenario.tests[str_test_name][
                     'current_spawns'] - self.scenario.tests[
@@ -48,16 +83,16 @@ class Spawner(object):
                 if int_spawns_difference < 0:
                     for int_counter in range(0, -int_spawns_difference):
                         str_test_path = self.scenario.get_test_path(str_test_name)
-                        str_id = random_id(16)
+                        str_id = utils.random_id(16)
                         print(str_id)
                         obj_spawn = Spawn(str_id, str_test_name,
                                           str_test_path, self.scenario.settings)
                         obj_spawn.start()
-                        self.scenario.tests[str_test_name]['threads'].append(obj_spawn)
+                        self.scenario.tests[str_test_name]['spawns'].append(obj_spawn)
 
                 elif int_spawns_difference > 0:
                     for int_counter in range(0, int_spawns_difference):
-                        obj_spawn = self.scenario.tests[str_test_name]['threads'].pop(0)
+                        obj_spawn = self.scenario.tests[str_test_name]['spawns'].pop(0)
                         obj_spawn.terminate()
                         obj_spawn.join()
                         print('Killed one')
