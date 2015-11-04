5d90df0aa1b73fe7bfc86bc247b400fdda3a7ee1
Stefano Romanò <rumix87@gmail.com>
2015-11-05 00:14:06 +0100

    Corrected error that prevents sender to send messages after first send

--- woodpecker/logging/sender.py
+++ woodpecker/logging/sender.py
@@ -11,27 +11,12 @@ class Sender(object):
 
     def __enter__(self, str_receiver_url, int_receiver_port, str_protocol):
         self.__initialize(str_receiver_url, int_receiver_port, str_protocol)
-        pass
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.__disconnect()
-
-    def __del__(self):
-        self.__disconnect()
-
-    def __disconnect(self):
-        self.socket.close()
 
     def __initialize(self, str_receiver_url, int_receiver_port, str_protocol):
         self.receiver_url = str_receiver_url
         self.receiver_port = int_receiver_port
         self.protocol = str_protocol
-
-        # Create based on protocol
-        if self.protocol == 'TCP':
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        elif self.protocol == 'UDP':
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket = None
 
     def send(self, str_data_type, dic_data):
         dic_payload = {'dataType': str_data_type, 'payload': dic_data}
@@ -40,11 +25,14 @@ class Sender(object):
         # Connects and send data
         try:
             if self.protocol == 'TCP':
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.socket.connect((self.receiver_url, self.receiver_port))
                 int_sent_bytes = self.socket.send(str_payload)
             elif self.protocol == 'UDP':
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 int_sent_bytes = self.socket.sendto(str_payload, (self.receiver_url, self.receiver_port))
             else:
+                self.socket = None
                 int_sent_bytes = -1
         finally:
             self.socket.close()
