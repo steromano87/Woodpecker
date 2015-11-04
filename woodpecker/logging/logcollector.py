3aff623f6df673af2f7a20f783eddf679cc293fe
Stefano Romanò <rumix87@gmail.com>
2015-11-04 12:23:13 +0100

    Aligned standard listening port for LogCollector Aligned  data type label

--- woodpecker/logging/logcollector.py
+++ woodpecker/logging/logcollector.py
@@ -16,20 +16,20 @@ class LogCollector(SocketServer.BaseRequestHandler):
         dic_message = json.loads(str_message)
         dic_payload = dic_message.get('payload', {})
 
-        if dic_message.get('logType') == 'transactionStart':
+        if dic_message.get('dataType') == 'transactionStart':
             self.dbwriter.write_transaction_start(dic_payload)
-        elif dic_message.get('logType') == 'transactionEnd':
+        elif dic_message.get('dataType') == 'transactionEnd':
             self.dbwriter.write_transaction_end(dic_payload)
-        elif dic_message.get('logType') == 'request':
+        elif dic_message.get('dataType') == 'request':
             self.dbwriter.write_request(dic_payload)
-        elif dic_message.get('logType') == 'spawns':
+        elif dic_message.get('dataType') == 'spawns':
             self.dbwriter.write_spawns_info(dic_payload)
-        elif dic_message.get('logType') == 'sysmonitor':
+        elif dic_message.get('dataType') == 'sysmonitor':
             self.dbwriter.write_sysmonitor_info(dic_payload)
-        elif dic_message.get('logType') == 'message':
+        elif dic_message.get('dataType') == 'message':
             self.dbwriter.write_message(dic_payload)
 
 if __name__ == '__main__':
-    obj_server = SocketServer.UDPServer(('localhost', 7777), LogCollector)
+    obj_server = SocketServer.UDPServer(('localhost', 7878), LogCollector)
     obj_server.result_file_path = "D:/Data/Varie/Programmi Python/Woodpecker/tests/results.sqlite"
     obj_server.serve_forever()
