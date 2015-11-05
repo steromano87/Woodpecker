daad9656eba07ebd07fcfba90a534ee3cbac6c0f
Stefano Romanò <rumix87@gmail.com>
2015-11-05 23:15:13 +0100

    Rewritten RemoteController class with integrated socket server

--- woodpecker/remotes/remotecontroller.py
+++ woodpecker/remotes/remotecontroller.py
@@ -1,8 +1,8 @@
-import SocketServer
+import socket
 import json
 import base64
 import tempfile
-import os
+import click
 
 import woodpecker.misc.utils as utils
 
@@ -15,68 +15,78 @@ from woodpecker.remotes.sysmonitor import Sysmonitor
 __author__ = 'romano'
 
 
-class RemoteController(SocketServer.StreamRequestHandler):
+class RemoteController(object):
 
-    def __init__(self, request, client_address, server):
-        # Init method of the father class
-        SocketServer.StreamRequestHandler.__init__(self, request, client_address, server)
+    def __init__(self, int_listening_port):
+        # Socket properties
+        self.socket = None
+        self.active = True
+        self.connection = None
+        self.client_address = None
+        self.data = None
+        self.buffer_size = 2 ** 20
 
         # Placeholder for scenario folder
-        if not self.scenario_folder:
-            self.scenario_folder = None
+        self.scenario_folder = None
 
         # Placeholder for scenario file
-        if not self.scenario_file_path:
-            self.scenario_file_path= None
+        self.scenario_file_path = None
 
         # Placeholder for scenario name
-        if not self.scenario_name:
-            self.scenario_name= None
+        self.scenario_name = None
 
         # Placeholder for scenario results file path
-        if not self.results_file_path:
-            self.results_file_path= None
+        self.results_file_path = None
 
         # Placeholder for controller IP address
-        if not self.controller_ip_address:
-            self.controller_ip_address= None
+        self.controller_ip_address = None
 
         # Standard port, this value will be eventually overridden
-        if not self.controller_port:
-            self.controller_port= 7878
+        self.controller_port = 7878
 
         # Spawn quota for local spawner
-        if not self.spawn_quota:
-            self.spawn_quota= None
+        self.spawn_quota = None
 
         # Spawner object
-        if not self.spawner:
-            self.spawner = None
+        self.spawner = None
 
         # Sysmonitor object
-        if not self.sysmonitor:
-            self.sysmonitor= None
+        self.sysmonitor = None
+
+        # Initialize the socket
+        self.__initialize(int_listening_port)
 
         return
 
-    def handle(self):
+    def __initialize(self, int_listening_port):
+        # Initialize socket
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listening_port = int_listening_port
+
+        self.socket.bind(('', self.listening_port))
+        self.socket.listen(3)
+
+        # Print message on screen
+        str_message = ' '.join(('Socket opened on port', str(self.listening_port)))
+        click.echo(str_message)
+
+    def __handle(self):
         # Read the messages and parse them into dict
-        str_message = self.rfile.readline().strip()
-        obj_message = json.loads(str_message)
+        obj_message = json.loads(self.data)
 
         # Switch action according to message type
-        if obj_message['dataType'] == 'start':
-            self.__initialize(obj_message['payload'])
+        if obj_message['dataType'] == 'setup':
+            self.__setup_scenario(obj_message['payload'])
+        elif obj_message['dataType'] == 'start':
             self.__start_scenario()
         elif obj_message['dataType'] == 'stop':
             pass
         elif obj_message['dataType'] == 'emergency_stop':
             pass
         elif obj_message['dataType'] == 'shutdown':
-            self.server.shutdown()
-            self.server.server_close()
+            self.shutdown()
 
-    def __initialize(self, dic_payload):
+    def __setup_scenario(self, dic_payload):
         # Get the Base64 encoded ZIP file from payload
         str_base64_zipped_scenario = dic_payload['scenarioBase64ZippedFolder']
         obj_zipped_scenario = StringIO(base64.b64decode(str_base64_zipped_scenario))
@@ -96,6 +106,9 @@ class RemoteController(SocketServer.StreamRequestHandler):
         self.results_file_path = utils.get_abs_path(dic_payload.get('resultsFile', './results/results.sqlite'),
                                                     self.scenario_folder)
 
+        # Create Sysmonitor
+        self.sysmonitor = Sysmonitor(self.controller_ip_address, self.controller_port)
+
     def __start_scenario(self):
         self.spawner = Spawner(self.controller_ip_address,
                                self.controller_port,
@@ -105,6 +118,20 @@ class RemoteController(SocketServer.StreamRequestHandler):
                                self.results_file_path,
                                self.spawn_quota)
 
+    def serve_forever(self):
+        click.secho('Waiting for controller connection...', fg='green', bold=True)
+        while self.active:
+            self.connection, self.client_address = self.socket.accept()
+            self.data = self.connection.recv(self.buffer_size)
+            self.__handle()
+
+        self.connection.close()
+
+    def shutdown(self):
+        str_message = ' '.join(('Remote controller gracefully closed by controller on', self.client_address))
+        click.echo(str_message)
+        self.active = False
+
 if __name__ == '__main__':
-    obj_server = SocketServer.TCPServer(('localhost', 7878), RemoteController)
+    obj_server = RemoteController(7878)
     obj_server.serve_forever()
