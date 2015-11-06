1f8ef8b31ab14f88bdaa3f9a655a609cb5604a94
Stefano Romanò <rumix87@gmail.com>
2015-11-06 16:20:12 +0100

    Refactored Spawn class Changed old style class instantiation with new utils function

--- woodpecker/remotes/spawn.py
+++ woodpecker/remotes/spawn.py
@@ -1,3 +1,5 @@
+import woodpecker.misc.utils as utils
+
 from woodpecker.misc.stoppablethread import StoppableThread
 
 __author__ = 'Stefano.Romano'
@@ -8,9 +10,7 @@ class Spawn(StoppableThread):
     def __init__(self, str_id, str_test_name, str_test_path, dic_settings):
         super(Spawn, self).__init__()
         self.ID = str_id
-        obj_module = __import__(str_test_path)
-        obj_class = getattr(obj_module, str_test_path)
-        self.testclass = obj_class(str_test_name, dic_settings)
+        self.testclass = utils.import_from_path(str_test_path, str_test_name)
         self.testclass.configure()
         self.testclass.transactions()
         self.armed = False
