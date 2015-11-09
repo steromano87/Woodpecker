08e917dc821d7d80b8dd89b02f4e71ad9c2faa56
Stefano Romanò <rumix87@gmail.com>
2015-11-09 12:40:43 +0100

    Fixed sysmonitor CPU usage calculation Now CPU usage is based on CPU times and should be more reliable

--- woodpecker/remotes/sysmonitor.py
+++ woodpecker/remotes/sysmonitor.py
@@ -23,12 +23,12 @@ class Sysmonitor(StoppableThread):
     def __initialize(self, str_receiver_url, int_receiver_port, int_polling_interval):
         self.sender = Sender(str_receiver_url, int_receiver_port, 'UDP')
         self.polling_interval = int_polling_interval
+        self.cpu_percent = CpuPercent()
 
-    @staticmethod
-    def send_sysmonitor_info():
+    def send_sysmonitor_info(self):
         tpl_memory = psutil.virtual_memory()
         dic_payload = {
-            'CPUperc': psutil.cpu_percent(interval=0.1),
+            'CPUperc': self.cpu_percent.update(),
             'memoryUsed': tpl_memory.total - tpl_memory.available,
             'memoryAvail': tpl_memory.available,
             'memoryPerc': tpl_memory.percent
@@ -41,3 +41,32 @@ class Sysmonitor(StoppableThread):
         while True:
             self.send_sysmonitor_info()
             time.sleep(self.polling_interval)
+
+
+class CpuPercent:
+    """Keep track of cpu usage."""
+
+    def __init__(self):
+        self.last = psutil.cpu_times()
+
+    def update(self):
+        """CPU usage is specific CPU time passed divided by total CPU time passed."""
+
+        last = self.last
+        current = psutil.cpu_times()
+
+        total_time_passed = sum([current.__dict__.get(key, 0) - last.__dict__.get(key, 0)
+                                 for key in current.__dict__.iterkeys()])
+
+        # only keeping track of system and user time
+        sys_time = current.system - last.system
+        usr_time = current.user - last.user
+
+        self.last = current
+
+        if total_time_passed > 0:
+            sys_percent = 100 * sys_time / total_time_passed
+            usr_percent = 100 * usr_time / total_time_passed
+            return round(sys_percent + usr_percent, 2)
+        else:
+            return 0
