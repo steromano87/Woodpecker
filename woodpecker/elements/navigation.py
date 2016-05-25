45553a5be925bfa7091f00646b9e0338ed75423c
Stefano Romanò <rumix87@gmail.com>
2016-05-26 01:40:53 +0200

    Fixed variable names

--- woodpecker/elements/navigation.py
+++ woodpecker/elements/navigation.py
@@ -15,17 +15,23 @@ class Navigation(object):
 
         # Navigation settings and scenario
         self.settings = kwargs.get('settings', {})
-        self.scenario_folder = kwargs.get('scenario_folder', os.getcwd())
+        self.scenario_folder = kwargs.get('scenario_folder',
+                                          utils.get_abs_path('/'.join(
+                                              (
+                                                  os.getcwd(),
+                                                  '..'
+                                              )
+                                          )))
 
         # Sender data
-        self.server_address = kwargs.get('server_address', 'localhost')
-        self.server_port = kwargs.get('server_port', 7878)
-        self.sender = Sender(self.server_address, self.server_port, 'UDP')
+        self.controller_address = kwargs.get('controller_address', 'localhost')
+        self.controller_port = kwargs.get('controller_port', 7878)
+        self.sender = Sender(self.controller_address, self.controller_port, 'UDP')
 
         # Internal variables
         self.thread_variables = {}
         self.test_transactions = []
-        self.spawn_id = None
+        self.pecker_id = None
         self.iteration = 1
         self.debug = False
 
@@ -43,12 +49,16 @@ class Navigation(object):
     def add_setting(self, str_default_key, str_value):
         """
         Add a setting value to the specified key
+        :param str_value:
+        :param str_default_key:
         """
         self.settings[str_default_key] = str_value
 
     def set_variable(self, str_name, str_value):
         """
         Add a variable to the thread variables dict
+        :param str_value:
+        :param str_name:
         """
         self.thread_variables[str_name] = str_value
 
@@ -58,13 +68,13 @@ class Navigation(object):
             'name': str_name,
             'path': str_abspath,
             'class': utils.import_from_path(str_abspath, str_name, {'navigation_name': self.name}),
-            'server_address': self.server_address,
-            'server_port': self.server_port
+            'controller_address': self.controller_address,
+            'controller_port': self.controller_port
         }
         self.test_transactions.append(obj_transaction)
 
-    def run(self, str_spawn_id=None):
-        self.spawn_id = str_spawn_id
+    def run(self, str_pecker_id=None):
+        self.pecker_id = str_pecker_id
 
         # Cycle through transactions
         for obj_transaction_item in self.test_transactions:
@@ -72,8 +82,8 @@ class Navigation(object):
             self.sender.send('transactionStart',
                              {
                                  'hostName': utils.get_ip_address(),
-                                 'spawnID': self.spawn_id,
-                                 'testName': self.name,
+                                 'peckerID': self.pecker_id,
+                                 'navigationName': self.name,
                                  'iteration': self.iteration,
                                  'transactionName': obj_transaction_item['name'],
                                  'startTimestamp': utils.get_timestamp()
@@ -82,14 +92,14 @@ class Navigation(object):
             # Run the transaction
             self.settings,\
                 self.thread_variables = \
-                obj_transaction_item['class'].run(self.spawn_id, self.iteration, self.settings, self.thread_variables)
+                obj_transaction_item['class'].run(self.pecker_id, self.iteration, self.settings, self.thread_variables)
 
             # Send transaction end message
             self.sender.send('transactionEnd',
                              {
                                  'hostName': utils.get_ip_address(),
-                                 'spawnID': self.spawn_id,
-                                 'testName': self.name,
+                                 'peckerID': self.pecker_id,
+                                 'navigationName': self.name,
                                  'iteration': self.iteration,
                                  'transactionName': obj_transaction_item['name'],
                                  'endTimestamp': utils.get_timestamp()
@@ -99,4 +109,4 @@ class Navigation(object):
 
         # Print line if in Debug mode
         if self.debug:
-            print('ID: ' + str(self.spawn_id) + ' Timestamp: ' + utils.get_timestamp())
+            print('ID: ' + str(self.pecker_id) + ' Timestamp: ' + utils.get_timestamp())
