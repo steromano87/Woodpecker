3a9f01c66b2d9d82353db63ea252ecb54b11a2ad
Stefano Romanò <rumix87@gmail.com>
2016-06-21 11:36:02 +0200

    Added spawner class to handle peckers management

---
+++ woodpecker/controllers/spawner.py
@@ -0,0 +1,72 @@
+import importlib
+import time
+
+from woodpecker.logging.log import Log
+from woodpecker.misc.stoppablethread import StoppableThread
+from woodpecker.options import Options
+
+
+class Spawner(StoppableThread):
+
+    def __init__(self, obj_scenario, **kwargs):
+        super(Spawner, self).__init__()
+
+        # Internal log
+        self.log = Log()
+
+        # Options
+        self.options = kwargs.get('options', Options())
+
+        # Loaded scenario
+        self._scenario = obj_scenario
+
+        # Peckers list
+        self._peckers = {}
+
+        # Elapsed time counter
+        self.elapsed_time = 0.0
+
+        # Spawning mode, to choose between threads and greenlet
+        self.spawning_mode = kwargs.get('spawning_method', self.options.get('execution', 'spawning_method'))
+        if self.spawning_mode == 'threads':
+            self.pecker_class = getattr(importlib.import_module('woodpecker.peckers.threadedpecker'), 'ThreadedPecker')
+        elif self.spawning_mode == 'greenlet':
+            self.pecker_class = getattr(importlib.import_module('woodpecker.peckers.greenletpecker'), 'GreenletPecker')
+
+        # Pecker handling mode, it may be active or passive
+        self.pecker_handling_mode = kwargs.get('pecker_handling_mode',
+                                               self.options.get('execution', 'pecker_handling_mode'))
+
+    def _initialize_peckers_list(self):
+        for str_nav_name in self._scenario.get_navigation_names():
+            self._peckers[str_nav_name] = []
+
+    def start_pecker_for(self, str_nav_name):
+        dic_navigation = self._scenario.get_navigation(str_nav_name)
+        obj_pecker = self.pecker_class(dic_navigation.get('max_iterations', None),
+                                       pecker_handling_mode=self.pecker_handling_mode)
+        obj_pecker.set_navigation(str_nav_name, dic_navigation.get('file'))
+        self._peckers[str_nav_name].append(obj_pecker)
+        self._peckers[str_nav_name][-1].start()
+
+    def stop_pecker_for(self, str_nav_name):
+        self._peckers[str_nav_name][0].mark_for_stop()
+        self._peckers[str_nav_name].pop(0)
+
+    def check_running_peckers(self):
+        if self.pecker_handling_mode == 'active':
+            for str_nav_name in self._scenario.get_navigation_names():
+                self._check_running_peckers_active(str_nav_name)
+            time.sleep(self.options.get('execution', 'pecker_status_active_polling_interval'))
+
+    def _check_running_peckers_active(self, str_nav_name):
+        int_scheduled_peckers = self._scenario.get_navigation_planned_peckers_at(self.elapsed_time)
+        int_running_peckers = len(self._peckers[str_nav_name])
+        int_peckers_diff = int_scheduled_peckers - int_running_peckers
+
+        if int_peckers_diff > 0:
+            for int_counter in range(1, int_peckers_diff):
+                self.start_pecker_for(str_nav_name)
+        elif int_peckers_diff < 0:
+            for int_counter in range(1, -int_peckers_diff):
+                self.stop_pecker_for(str_nav_name)
