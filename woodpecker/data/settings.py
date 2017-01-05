55b575ec6d646d85e083546140b5b4a34f486bd9
Stefano Romanò <rumix87@gmail.com>
2017-01-05 16:50:45 +0100

    Added validation for settings file and on entries update

--- woodpecker/data/settings.py
+++ woodpecker/data/settings.py
@@ -1,14 +1,16 @@
 import os
 import six
 import abc
+import pprint
 
 import yaml
+import cerberus
 
 
 class Settings(object):
     """
     Abstract class that is subclassed by the settings specific
-     to each transaction type
+    to each transaction type
     """
     __metaclass__ = abc.ABCMeta
 
@@ -19,15 +21,24 @@ class Settings(object):
         # Settings data
         self._data = {}
 
+        # Validation mask
+        self._validation_mask = {} \
+            if not self._validation_mask \
+            else self._validation_mask
+
+        # Internal validator
+        self._validator = cerberus.Validator(self._validation_mask,
+                                             purge_unknown=True)
+
         # Load settings data
         self.load()
 
     def load(self):
         if os.path.isfile(self._peckerfile):
             with open(self._peckerfile, 'r') as fp:
-                self._data = yaml.safe_load(fp)
+                self._data = self._validator.validated(yaml.safe_load(fp))
         else:
-            self._data = self.default_settings()
+            self._data = self._validator.validated({})
 
     def save(self):
         with open(self._peckerfile, 'w') as pf:
@@ -48,69 +59,187 @@ class Settings(object):
             ))
 
     def set(self, section, entry, value):
-        if section in six.viewkeys(self._data):
-            if entry in six.viewkeys(self._data.get(section)):
-                self._data[section][entry] = value
-            else:
-                raise KeyError(
-                    'The entry {entry} does not exist ',
-                    'in section {section}'.format(entry=entry, section=section)
-                )
+        if self._validator.validate({section: {entry: value}}):
+            self._data.update({section: {entry: value}})
         else:
-            raise KeyError('The section {section} does not exist'.format(
-                section=section
+            raise ValueError('Error in input values:\n{error_list}'.format(
+                error_list=pprint.pformat(self._validator.errors, indent=2)
             ))
 
     def dump(self):
         return self._data
 
+    def validate(self):
+        try:
+            return self._validator.validate(self._data), \
+                   pprint.pprint(self._validator.errors)
+        except cerberus.DocumentError as error:
+            return False, error.message
+
     def extend(self, additional_settings):
         if isinstance(additional_settings, Settings):
-            self._data.update(additional_settings.dump())
+            additional_settings.load()
+            bool_is_valid, str_message = additional_settings.validate()
+            if bool_is_valid:
+                self._validation_mask.update(
+                    additional_settings._validation_mask
+                )
+            else:
+                raise ValueError('Given settings are not self-consistent:\n'
+                                 '{errors}'.format(errors=str_message)
+                                 )
         else:
             raise TypeError('Wrong type: provided settings are not an '
                             'instance of Woodpecker settings')
-
-    @staticmethod
-    @abc.abstractmethod
-    def default_settings():
-        pass
 
 
 class BaseSettings(Settings):
     """
     Base settings for Woodpecker
     """
-    @staticmethod
-    def default_settings():
-        return {
+    def __init__(self, peckerfile='Peckerfile'):
+        self._validation_mask = {
             'timing': {
-                'skip_think_time': False,
-                'max_think_time': 5.0,
-                'think_time_after_setup': 0.0,
-                'think_time_between_transactions': 0.0,
-                'think_time_before_teardown': 0.0,
-                'think_time_between_iterations': 0.0
+                'type': 'dict',
+                'schema': {
+                    'skip_think_time': {
+                        'type': 'boolean'
+                    },
+                    'max_think_time': {
+                        'type': 'number',
+                        'min': 0.0,
+                        'coerce': float
+                    },
+                    'think_time_after_setup': {
+                        'type': 'number',
+                        'min': 0.0,
+                        'coerce': float
+                    },
+                    'think_time_between_transactions': {
+                        'type': 'number',
+                        'min': 0.0,
+                        'coerce': float
+                    },
+                    'think_time_before_teardown': {
+                        'type': 'number',
+                        'min': 0.0,
+                        'coerce': float
+                    },
+                    'think_time_between_iterations': {
+                        'type': 'number',
+                        'min': 0.0,
+                        'coerce': float
+                    }
+                },
+                'default': {
+                    'skip_think_time': False,
+                    'max_think_time': 5.0,
+                    'think_time_after_setup': 0.0,
+                    'think_time_between_transactions': 0.0,
+                    'think_time_before_teardown': 0.0,
+                    'think_time_between_iterations': 0.0
+
+                }
             },
             'network': {
-                'controller_port': 7877,
-                'max_pending_connections': 4
+                'type': 'dict',
+                'schema': {
+                    'controller_port': {
+                        'type': 'integer',
+                        'min': 1,
+                        'max': 65536
+                    },
+                    'max_pending_connections': {
+                        'type': 'integer',
+                        'min': 1
+                    }
+                },
+                'default': {
+                    'controller_port': 7877,
+                    'max_pending_connections': 4
+
+                }
             },
             'spawning': {
-                'spawning_mode': 'threads',
-                'pecker_handling_mode': 'passive',
-                'pecker_status_active_polling_interval': 0.1,
-                'spawners': [
-                    'localhost'
-                ]
+                'type': 'dict',
+                'schema': {
+                    'spawning_mode': {
+                        'type': 'string',
+                        'allowed': [
+                            'processes',
+                            'threads',
+                            'greenlets'
+                        ]
+                    },
+                    'pecker_handling_mode': {
+                        'type': 'string',
+                        'allowed': [
+                            'passive',
+                            'active'
+                        ]
+                    },
+                    'pecker_status_active_polling_interval': {
+                        'type': 'number',
+                        'min': 0.0,
+                        'coerce': float
+                    },
+                    'spawners': {
+                        'type': 'list',
+                        'schema': {
+                            'type': 'string'
+                        },
+                        'coerce': list
+                    }
+                },
+                'default': {
+                    'spawning_mode': 'threads',
+                    'pecker_handling_mode': 'passive',
+                    'pecker_status_active_polling_interval': 0.1,
+                    'spawners': [
+                        'localhost'
+                    ]
+
+                }
             },
             'logging': {
-                'max_entries_before_flush': 10,
-                'max_interval_before_flush': 30.0,
-                'results_file': 'results',
-                'sysmonitor_polling_interval': 5.0
+                'type': 'dict',
+                'schema': {
+                    'max_entries_before_flush': {
+                        'type': 'integer',
+                        'min': 0
+                    },
+                    'max_interval_before_flush': {
+                        'type': 'number',
+                        'min': 0,
+                        'coerce': float
+                    },
+                    'results_file': {
+                        'type': 'string',
+                        'regex': r"[^\/\\]+"
+                    },
+                    'sysmonitor_polling_interval': {
+                        'type': 'number',
+                        'min': 0,
+                        'coerce': float
+                    }
+                },
+                'default': {
+                    'max_entries_before_flush': 10,
+                    'max_interval_before_flush': 30.0,
+                    'results_file': 'results.sqlite',
+                    'sysmonitor_polling_interval': 5.0
+                }
             },
             'runtime': {
-                'raise_error_if_variable_not_defined': False
+                'type': 'dict',
+                'schema': {
+                    'raise_error_if_variable_not_defined': {
+                        'type': 'boolean'
+                    }
+                },
+                'default': {
+                    'raise_error_if_variable_not_defined': False
+                }
             }
         }
+        super(BaseSettings, self).__init__(peckerfile)
