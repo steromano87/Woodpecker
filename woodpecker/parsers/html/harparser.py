12e1b9909a75758c7ba6bbc6149c9b5f1ee055be
Stefano Romanò <rumix87@gmail.com>
2017-03-15 17:45:09 +0100

    Added calculation of elapsed time from the end of the previous one

--- woodpecker/parsers/html/harparser.py
+++ woodpecker/parsers/html/harparser.py
@@ -93,4 +93,21 @@ class HarParser(BaseParser):
             request['elapsed_from_previous'] = \
                 request['elapsed_from_start']
 
+        # If elapsed is negative, force it to zero
+        request['elapsed_from_previous'] = request['elapsed_from_previous'] \
+            if request['elapsed_from_previous'] > 0 else 0.0
+
+        # Get request elapsed from end of previous request (if any)
+        try:
+            request['elapsed_from_end_of_previous'] = \
+                request['elapsed_from_previous'] - \
+                self._parsed['entries'][-1]['request']['duration']
+        except (KeyError, IndexError):
+            request['elapsed_from_end_of_previous'] = 0.0
+
+        # If elapsed is negative, force it to zero
+        request['elapsed_from_end_of_previous'] = \
+            request['elapsed_from_end_of_previous'] \
+            if request['elapsed_from_end_of_previous'] > 0 else 0.0
+
         return request
