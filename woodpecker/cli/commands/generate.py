5b95c78c1b6ba95c44a0afe388bb16a95a6bd8e4
Stefano Romanò <rumix87@gmail.com>
2017-05-22 17:17:23 +0200

    Added command stub for new sequence generation

---
+++ woodpecker/cli/commands/generate.py
@@ -0,0 +1,68 @@
+import click
+
+from pkg_resources import iter_entry_points
+from click_plugins import with_plugins
+
+
+@with_plugins(iter_entry_points('woodpecker.generate'))
+@click.group(short_help='Creates a new entry')
+@click.pass_context
+def generate(ctx):
+    """
+    Generates a new Woodpecker item in the proper folder
+    """
+    pass
+
+
+@with_plugins(iter_entry_points('woodpecker.generate.sequence'))
+@generate.group(short_help='Generates a new sequence')
+@click.option('--name', '-f', help='Name of the sequence to be generated')
+@click.option('--file', '-f',
+              type=click.Path(exists=False, dir_okay=True, writable=True),
+              help='Name of the file that contains the generated sequence. '
+                   'If no name is provided, the file will be named after the '
+                   'sequence name and will be placed under the default '
+                   '"sequences" folder.',
+              default=None)
+@click.option('--type', '-t',
+              help='Sequence class to be used',
+              type=(str, str),
+              default=(None, None))
+@click.pass_context
+def sequence(ctx, name, file, type):
+    """
+    Generates a new sequence from the given source.
+    If the sequence type is provided, all the required include statements 
+    will be automatically added, else the type of the sequence will be 
+    determined by the issued command
+    """
+    ctx.obj['SEQUENCE_NAME'] = name or 'NewSequence'
+    ctx.obj['SEQUENCE_FILE'] = file or ''.join(
+        (
+            ctx.obj['WORKDIR'],
+            '/sequences/',
+            ctx.obj['SEQUENCE_NAME'],
+            'py')
+    )
+    ctx.obj['SEQUENCE_CLASS'] = type
+
+
+@sequence.command(short_help='Generates a new, empty sequence')
+@click.pass_context
+def from_scratch(ctx):
+    """
+    Generates a new, empty sequence.
+    If the sequence type is provided, all the required include statements 
+    will be automatically added, else a BaseSequence will be created
+    """
+    pass
+
+
+@sequence.command(short_help='Generates a new sequence from HAR file')
+@click.argument('har_files', type=click.File('r'), nargs=-1)
+@click.pass_context
+def from_har(ctx, har_files):
+    """
+    Generates a new sequence (HttpSequence) from a HAR file
+    """
+    pass
--- woodpecker/cli/woodpecker.py
+++ woodpecker/cli/woodpecker.py
@@ -5,8 +5,10 @@ from pkg_resources import iter_entry_points
 from click_plugins import with_plugins
 
 from .._version import get_versions
+
 from .commands.init import init
 from .commands.update import update
+from commands.generate import generate
 
 
 @with_plugins(iter_entry_points('woodpecker.main'))
@@ -26,6 +28,7 @@ def woodpecker(ctx, workdir):
 
 woodpecker.add_command(init)
 woodpecker.add_command(update)
+woodpecker.add_command(generate)
 
 
 if __name__ == '__main__':
