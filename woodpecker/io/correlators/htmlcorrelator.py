95526b5caf4b1643ac89111b7f44356ceb55a861
Stefano Romanò <rumix87@gmail.com>
2017-05-27 01:11:07 +0200

    Addedbase class and HTTP correlator for requests generation The parameters correlation is still to be implemented

---
+++ woodpecker/io/correlators/htmlcorrelator.py
@@ -0,0 +1,55 @@
+import six
+
+from collections import OrderedDict
+
+from woodpecker.io.correlators.basecorrelator import BaseCorrelator
+
+
+class HtmlCorrelator(BaseCorrelator):
+    def __init__(self, parsed_entries):
+        super(HtmlCorrelator, self).__init__(parsed_entries)
+
+        # List of referers
+        self.referers = set()
+
+        # Call hierarchy
+        self._correlated = OrderedDict()
+
+    def correlate(self):
+        self._get_referers()
+        self._calculate_call_hierarchy()
+        return self._correlated
+
+    def _get_referers(self):
+        # Start reading the loaded entries and getting the Referers
+        for entry in self._parsed_entries.get('entries', []):
+            headers = entry.get('request', {}).get('headers', {})
+
+            # If the call has a referer, add the referer to list
+            for header_name, header_value in six.viewitems(headers):
+                if header_name.lower() in ('referer', 'referrer'):
+                    self.referers.add(header_value)
+
+    def _calculate_call_hierarchy(self):
+        # Cycle through referers and entries and get the top calls
+        for entry in self._parsed_entries.get('entries', []):
+            entry_url = entry.get('request', {}).get('url', '')
+            if entry_url in self.referers:
+                self._correlated[entry_url] = entry
+                self._correlated[entry_url]['resources'] = []
+            else:
+                headers = entry.get('request', {}).get('headers', {})
+                is_referred = False
+                for header_name, header_value in six.viewitems(headers):
+                    if header_name.lower() in ('referer', 'referrer') \
+                            and header_value in six.viewkeys(
+                                self._correlated):
+                        self._correlated[header_value]['resources'].append(
+                            entry
+                        )
+                        is_referred = True
+
+                if not is_referred:
+                    entry_url = entry.get('request', {}).get('url', '')
+                    self._correlated[entry_url] = entry
+                    self._correlated[entry_url]['resources'] = []
