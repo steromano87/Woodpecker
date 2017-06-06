bb0586e5353ea491f03cf56bb9e47f634d7dc04c
Stefano Romanò <rumix87@gmail.com>
2017-06-06 16:47:12 +0200

    Added prepended and appended events to handle dependency

--- woodpecker/io/correlators/event.py
+++ woodpecker/io/correlators/event.py
@@ -12,13 +12,20 @@ class Event(object):
         self.id = uuid.uuid4()
 
         # Related events
-        self.related_events = EventCollection()
+        self.prepended_events = EventCollection()
+        self.appended_events = EventCollection()
 
-    def add_related_event(self, event):
-        self.related_events.add_event(event)
+    def prepend_event(self, event):
+        self.prepended_events.add_event(event)
 
-    def has_related_events(self):
-        return self.related_events.event_count() > 0
+    def append_event(self, event):
+        self.appended_events.add_event(event)
+
+    def has_prepended_events(self):
+        return self.prepended_events.event_count() > 0
+
+    def has_appended_events(self):
+        return self.appended_events.event_count() > 0
 
 
 class EventCollection(object):
@@ -32,20 +39,33 @@ class EventCollection(object):
     def _increase_counter(self):
         self._event_counter += 1
 
-    def add_event(self, event, related_event=None):
+    def add_event(self, event):
         # Raise exception if teh entry is not an event
         if not isinstance(event, Event):
             raise TypeError('Only events can be added to an Event Collection')
 
-        # If no related event is provided, add the event as main event
-        if related_event is None:
-            self._events[event.id] = event
-        else:
-            try:
-                self._events[related_event].add_related_event(event)
-            except KeyError:
-                raise KeyError('Event {id} not found'.format(id=related_event))
+        self._events[event.id] = event
         self._increase_counter()
+
+    def prepend_event(self, main_event_id, prepended_event):
+        # Raise exception if teh entry is not an event
+        if not isinstance(prepended_event, Event):
+            raise TypeError('Only events can be added to an Event Collection')
+
+        try:
+            self._events[main_event_id].prepend_event(prepended_event)
+        except KeyError:
+            raise KeyError('Event {id} not found'.format(id=main_event_id))
+
+    def append_event(self, main_event_id, appended_event):
+        # Raise exception if teh entry is not an event
+        if not isinstance(appended_event, Event):
+            raise TypeError('Only events can be added to an Event Collection')
+
+        try:
+            self._events[main_event_id].append_event(appended_event)
+        except KeyError:
+            raise KeyError('Event {id} not found'.format(id=main_event_id))
 
     def event_count(self):
         return self._event_counter
