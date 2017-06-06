a78670ec39909452d2c78c939b09a9c32e7e47ed
Stefano Romanò <rumix87@gmail.com>
2017-06-06 16:49:17 +0200

    Removed useless file dumps

--- woodpecker/io/parsers/baseparser.py
+++ woodpecker/io/parsers/baseparser.py
@@ -22,24 +22,6 @@ class BaseParser(object):
     def parse(self):
         pass
 
-    def parse_to_file(self, filename=None, binary=True):
-        output_file_path = \
-            filename or \
-            '.'.join((
-                os.path.splitext(self._file_path)[0],
-                'pel'
-            ))
-        with open(output_file_path, 'w+') as fp:
-            if binary:
-                msgpack.dump(self.parse(),
-                             fp,
-                             default=functions.encode_datetime)
-            else:
-                simplejson.dump(self.parse(),
-                                fp,
-                                default=functions.encode_datetime,
-                                indent='\t')
-
     @abc.abstractmethod
     def _import_file(self, filename):
         pass
