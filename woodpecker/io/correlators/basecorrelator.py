7bf77ecc05ecf0453405b22e976f837a259dc75c
Stefano Romanò <rumix87@gmail.com>
2017-06-07 17:23:36 +0200

    Added event collection to base correlator class

--- woodpecker/io/correlators/basecorrelator.py
+++ woodpecker/io/correlators/basecorrelator.py
@@ -1,10 +1,6 @@
 import abc
-import os
 
-import msgpack
-import simplejson
-
-import woodpecker.misc.functions as functions
+from woodpecker.io.correlators.event import Event, EventCollection
 
 
 class BaseCorrelator(object):
@@ -15,6 +11,17 @@ class BaseCorrelator(object):
 
         self._parsed_entries = parsed_entries
 
+        # Events
+        self.events = EventCollection()
+
     @abc.abstractmethod
     def correlate(self):
         pass
+
+    def _add_think_time_event(self, think_time):
+        event = Event('think_time')
+        event.data.update({'think_time': think_time})
+        self.events.add_event(event)
+
+    def _add_sequence_break(self):
+        self.events.add_event(Event('sequence_break'))
