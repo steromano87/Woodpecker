49e152066176429c53f23f8100f82ed893afb86f
Stefano Romanò <rumix87@gmail.com>
2017-06-09 16:41:50 +0200

    Added methods to create steps for HttpTransaction from event collection

--- woodpecker/io/generators/html/httpsequencegenerator.py
+++ woodpecker/io/generators/html/httpsequencegenerator.py
@@ -1,20 +1,27 @@
-import copy
 import six
 
+from copy import deepcopy
+
 from woodpecker.io.generators.basegenerator import BaseGenerator
+from woodpecker.io.generators.basegenerator import CommandGenerator
 
 
 class HttpSequenceGenerator(BaseGenerator):
+    def __init__(self, event_collection):
+        super(HttpSequenceGenerator, self).__init__(event_collection)
+
+        # Added async active flag to handle async pool
+        self.async_pool_active = False
+
     def generate(self,
                  filename,
                  sequence_name='NewSequence',
-                 folder='sequences',
-                 **kwargs):
+                 folder='sequences'):
         self.base_sequence_name = sequence_name
 
         self._generate_headers()
         self._generate_class_definition(sequence_name)
-        self._generate_calls(**kwargs)
+        self._generate_calls()
 
         self._dump_buffers(filename, folder=folder)
 
@@ -34,124 +41,159 @@ class HttpSequenceGenerator(BaseGenerator):
     def _generate_steps_method_definition(self):
         self.buffer.write('    def steps(self):\n')
 
-    def _generate_calls(self,
-                        group_resources=True,
-                        auto_assert=True,
-                        think_time_threshold=2.0,
-                        new_sequence_threshold=5.0,
-                        async_request_threshold=0.1):
+    def _generate_calls(self):
         # Generate steps call
         self._generate_steps_method_definition()
 
-        previous_entry = None
-        for entry in six.viewvalues(self._parsed_entries):
-            self._generate_single_entry(entry,
-                                        previous_entry,
-                                        group_resources,
-                                        auto_assert,
-                                        think_time_threshold,
-                                        new_sequence_threshold,
-                                        async_request_threshold,
-                                        False)
-            self.buffer.write('\n')
-            previous_entry = copy.deepcopy(entry)
-            if not group_resources and len(entry.resources) > 0:
-                self.buffer.write('        self.start_async_pool()\n\n')
-                for resource in entry.resources:
-                    self._generate_single_entry(resource,
-                                                previous_entry,
-                                                group_resources,
-                                                auto_assert,
-                                                think_time_threshold,
-                                                new_sequence_threshold,
-                                                async_request_threshold,
-                                                True)
-                self.buffer.write('        self.end_async_pool()\n\n')
+        self._handle_event_list(self.events.all_events())
 
         # In the end, clean and store the current buffer
         self._clean_buffer()
 
-    def _generate_single_entry(self,
-                               entry,
-                               previous_entry,
-                               group_resources,
-                               auto_assert,
-                               think_time_threshold,
-                               new_sequence_threshold,
-                               async_request_threshold,
-                               _async):
-        # Determine if the previous call has an elapsed higher than
-        # the new sequence threshold. In such case, start a new sequence
-        if entry.timings.elapsed[
-                'from_end_of_previous'] / 1000 > new_sequence_threshold:
-            self._clean_buffer()
-            sequence_name = '{basename}_{index}'.format(
-                basename=self.base_sequence_name,
-                index=len(self.buffer_list)
-            )
-            self._generate_headers()
-            self._generate_class_definition(sequence_name)
-            self._generate_steps_method_definition()
-
-        # If the elapsed is greater than the think time threshold,
-        # set the think time
-        elif entry.timings.elapsed[
-                'from_end_of_previous'] / 1000 > think_time_threshold:
-            self.buffer.write('\n\n')
-            self.buffer.write('        self.think_time({amount})'.format(
-                amount=entry.timings.elapsed['from_end_of_previous'] * 1000
-            ))
-            self.buffer.write('\n\n')
-
-        # Write the call according to the method
-        if entry.method.upper() in ('GET', 'POST', 'PUT', 'PATCH', 'DELETE'):
-            self.buffer.write(
-                '        self.{async}{method}('.format(
-                    async='async_' if _async else '',
-                    method=entry.method.lower()
+    def _handle_event_list(self, event_list):
+        for event in event_list:
+            if event.type in ('http_request', 'http_load_page'):
+                self._generate_load_page_entry(event)
+            elif event.type == 'http_load_resource':
+                self._generate_load_resource_entry(event)
+            elif event.type == 'http_async_request':
+                self._generate_async_request_entry(event)
+            elif event.type == 'http_set_headers':
+                self._generate_set_header_entry(event)
+            elif event.type == 'http_set_cookie':
+                self._generate_set_cookie_entry(event)
+            elif event.type == 'think_time':
+                self._generate_think_time_entry(event)
+            elif event.type == 'sequence_break':
+                self._clean_buffer()
+                sequence_name = '{basename}_{index}'.format(
+                    basename=self.base_sequence_name,
+                    index=len(self.buffer_list)
                 )
-            )
-            # Write the URL
-            self.buffer.write("'{url}'\n".format(url=entry.url))
-            self.buffer.write(', ')
+                self._generate_headers()
+                self._generate_class_definition(sequence_name)
+
+        if self.async_pool_active:
+            async_pool = CommandGenerator('self.end_async_pool', indents=2)
+            self.buffer.write(async_pool.generate_command())
+            self.buffer.write('\n\n')
+            self.async_pool_active = False
+
+    def _generate_http_request_entry(self,
+                                     event,
+                                     is_resource=False,
+                                     is_async=False):
+        # Check for prepended events
+        if event.has_prepended_events():
+            self._handle_event_list(event.prepended_events.all_events())
+
+        if event.data['method'].upper() \
+                in ('GET', 'POST', 'PUT', 'PATCH', 'DELETE'):
+            if is_async:
+                load_page = CommandGenerator(
+                    'self.async_{method}'.format(
+                        method=event.data['method'].lower()),
+                    indents=2
+                )
+            else:
+                load_page = CommandGenerator(
+                    'self.{method}'.format(
+                        method=event.data['method'].lower()),
+                    indents=2
+                )
+            load_page.add_argument(event.data['url'])
         else:
-            self.buffer.write(
-                '        self.http_request('
-            )
-            # Write the URL
-            self.buffer.write("'{url}'\n".format(url=entry.url))
-            self.buffer.write(", method='{method}', ".format(
-                method=entry.method.upper()
-            ))
+            if is_async:
+                load_page = CommandGenerator(
+                    'self.async_http_request',
+                    indents=2
+                )
+            else:
+                load_page = CommandGenerator(
+                    'self.http_request',
+                    indents=2
+                )
+            load_page.add_argument(event.data['url'])
+            load_page.add_named_argument('method',
+                                         event.data['method'].lower())
 
-        # Check if URL is resource
-        if entry.mime_type() in (
-            'text/css',
-            'application/javascript',
-            'image/png'
-            'image/jpeg'
-            'image/gif',
-            'image/tiff'
-        ):
-            self.buffer.write('is_resource=True, ')
+        # Add headers
+        if len(event.data['headers']) > 0:
+            load_page.add_named_argument('headers', event.data['headers'])
 
-        # If auto assert is true, perform some checks on the entry response
-        # text to perform a semi-intelligent assert
-        # TODO: add automatic assert feature
+        # Add query string params
+        if len(event.data['query_params']) > 0:
+            load_page.add_named_argument('params', event.data['query_params'])
 
-        # If group resources is true, group them in the with_resources clause
-        if group_resources and len(entry.resources) > 0:
-            self.buffer.write('with_resources=(')
-            for resource in entry.resources:
-                if resource.method == 'GET':
-                    self.buffer.write("'{url}'".format(url=resource.url))
-                else:
-                    self.buffer.write("('{url}', '{method}')".format(
-                        url=resource.url,
-                        method=resource.method
-                    ))
-                self.buffer.write(',')
-            self.buffer.write(')')
+        # Add POST form data
+        if len(event.data['form_data']) > 0:
+            load_page.add_named_argument('data', event.data['form_data'])
 
-        # Close the method call
-        self.buffer.write(')\n\n')
+        # Mark if resource or not
+        load_page.add_named_argument('is_resource', is_resource)
+
+        # Add command to buffer
+        self.buffer.write(load_page.generate_command())
+
+        # Check for appended events
+        if event.has_appended_events():
+            self._handle_event_list(event.appended_events.all_events())
+
+    def _generate_load_page_entry(self, event):
+        if self.async_pool_active:
+            async_pool = CommandGenerator('self.end_async_pool', indents=2)
+            self.buffer.write(async_pool.generate_command())
+            self.buffer.write('\n\n')
+            self.async_pool_active = False
+
+        self._generate_http_request_entry(event,
+                                          is_async=False,
+                                          is_resource=False)
+
+    def _generate_load_resource_entry(self, event):
+        if not self.async_pool_active:
+            async_pool = CommandGenerator('self.start_async_pool', indents=2)
+            self.buffer.write(async_pool.generate_command())
+            self.buffer.write('\n\n')
+            self.async_pool_active = True
+
+        self._generate_http_request_entry(event,
+                                          is_async=True,
+                                          is_resource=True)
+
+    def _generate_async_request_entry(self, event):
+        if not self.async_pool_active:
+            async_pool = CommandGenerator('self.start_async_pool', indents=2)
+            self.buffer.write(async_pool.generate_command())
+            self.buffer.write('\n\n')
+            self.async_pool_active = True
+
+        self._generate_http_request_entry(event,
+                                          is_async=True,
+                                          is_resource=False)
+
+    def _generate_set_header_entry(self, event):
+        for header_name, header_value in six.iteritems(event.data):
+            set_header = CommandGenerator('self.set_header', indents=2)
+            set_header.add_argument(header_name)
+            set_header.add_argument(header_value)
+            self.buffer.write(set_header.generate_command())
+            self.buffer.write('\n\n')
+
+    def _generate_set_cookie_entry(self, event):
+        set_cookie = CommandGenerator('self.set_cookie', indents=2)
+        cookie_specs = deepcopy(event.data)
+        set_cookie.add_argument(cookie_specs['name'])
+        del cookie_specs['name']
+        set_cookie.add_argument(cookie_specs['value'])
+        del cookie_specs['value']
+
+        for spec_name, spec_value in six.iteritems(cookie_specs):
+            set_cookie.add_named_argument(spec_name, spec_value)
+
+        self.buffer.write(set_cookie.generate_command())
+
+    def _generate_think_time_entry(self, event):
+        think_time = CommandGenerator('self.think_time')
+        think_time.add_argument(event.data['think_time'])
+        self.buffer.write(think_time.generate_command())
